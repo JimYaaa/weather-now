eacc8fc77d1a44a43838551faa61551d8fc3db96
JimYaaa <jimyeh.12@gmail.com>
2024-06-29 17:24:10 +0800

    test: 💍 Add test for check type of celsiusToFahrenheit function

--- utils/celsiusToFahrenheit.test.ts
+++ utils/celsiusToFahrenheit.test.ts
@@ -1,9 +1,14 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, expectTypeOf } from 'vitest'
 import celsiusToFahrenheit from './celsiusToFahrenheit'
 
 describe('Celsius To Fahrenheit', () => {
     it('convert', () => {
         expect(celsiusToFahrenheit(1)).toBe(33)
         expect(celsiusToFahrenheit(30)).toBe(86)
+    })
+
+    it('type check', () => {
+        expectTypeOf(celsiusToFahrenheit).toBeFunction()
+        expectTypeOf(celsiusToFahrenheit).parameter(0).toMatchTypeOf<number>()
     })
 })
